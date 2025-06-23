33bff8d49f75fb0036f23ecdc948baba55d4501b
Luke Baertlein <luke.baertlein.oldsmumn@gmail.com>
2025-06-23 09:50:32 -0400

    modfied delete-all to delete-some

--- code/delete_some_saved_data.R
+++ code/delete_some_saved_data.R
@@ -1,5 +1,6 @@
 library(httr)
 library(jsonlite)
+library(tidyverse)
 
 # ---- Load Dropbox credentials ----
 dropbox_keys <- rio::import("data/account_info.xlsx", which = "Dropbox") %>%
@@ -11,6 +12,7 @@ DROPBOX_UPLOAD_PATH   <- dropbox_keys[["DROPBOX_UPLOAD_PATH"]]
 DROPBOX_REFRESH_TOKEN <- dropbox_keys[["DROPBOX_REFRESH_TOKEN"]]
 DROPBOX_APP_KEY       <- dropbox_keys[["DROPBOX_APP_KEY"]]
 DROPBOX_APP_SECRET    <- dropbox_keys[["DROPBOX_APP_SECRET"]]
+DROPBOX_DOWNLOAD_URL  <- dropbox_keys[["DROPBOX_DOWNLOAD_URL"]]
 
 # ---- Refresh token ----
 token_res <- httr::POST(
@@ -29,10 +31,24 @@ if (is.null(access_token)) {
   stop("Failed to refresh Dropbox token.")
 }
 
-# ---- Create and upload empty tibble ----
+# Get completed data ------------------------------------------------------
 temp_file <- tempfile(fileext = ".rds")
-empty_df <- tibble::tibble()  # or list() for your app if it expects a list format
-saveRDS(empty_df, temp_file)
+httr::GET(DROPBOX_DOWNLOAD_URL, httr::write_disk(temp_file, overwrite = TRUE))
+raw_data <- readRDS(temp_file)
+
+# Remove specified addresses ----------------------------------------------
+to_remove <- rio::import("data/to_remove.xlsx")
+
+rows_to_remove <- raw_data %>%
+  filter(sn %in% to_remove$Remove)
+
+raw_data_sub1 <- raw_data %>%
+  filter(!(sn %in% to_remove$Remove))
+
+nrow(raw_data_sub1) == nrow(raw_data) - nrow(to_remove)
+
+temp_file <- tempfile(fileext = ".rds")
+saveRDS(raw_data_sub1, temp_file)
 
 upload_res <- httr::POST(
   url = DROPBOX_UPLOAD_URL,
@@ -52,7 +68,7 @@ upload_res <- httr::POST(
 )
 
 if (upload_res$status_code == 200) {
-  message("Successfully cleared Dropbox data.")
+  message("Successfully removed specified addresses.")
 } else {
   stop("Dropbox upload failed: ", content(upload_res, "text"))
 }
